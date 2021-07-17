a711e54af7d5bca3b05c017a789bb951e5290447
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-07-17 15:01:46 +0200

    a

--- include/ph_network/network.hpp
+++ include/ph_network/network.hpp
@@ -16,3 +16,161 @@ auto write (Socket auto&& socket, String auto&& msg)
         total_bytes_written += socket.write_some (boost::asio::buffer (msg.c_str() + total_bytes_written, msg.length() - total_bytes_written));
     }
 }
+
+
+
+namespace ph
+{
+    
+    
+    
+    
+    
+#define _KEYBOARD \
+X (newline, '\n') \
+X (tab, '\t') \
+X (backspace, '\b') \
+X (backslash, '\\') \
+X (nullChar, '\0')
+    enum struct keyboard
+    {
+#define X(a, ...) a,
+        _KEYBOARD
+#undef X
+        undefined
+//        newline,
+//        tab,
+//        backspace,
+//        backslash,
+//        nullChar
+//        auto operator=(keyboard const& o)
+//        {
+//            return
+//        }
+    };
+    
+    auto operator== (char const& c, keyboard const& k) -> bool
+    {
+        #define X(a, b, ...) \
+                if (k == keyboard::a and c == b) return true;
+        _KEYBOARD
+        #undef X
+                else
+                {
+                    return false;
+                }
+    }
+    
+    auto operator== (keyboard const& k, char const& c) -> bool
+    {
+
+#define X(a, b, ...) \
+        if (k == keyboard::a and c == b) return true;
+
+_KEYBOARD
+#undef X
+        else
+        {
+            return false;
+        }
+    }
+    
+    namespace network
+    {
+        
+//        template <typename protocol>
+//        struct application_layer;
+//
+//        template <>
+//        struct application_layer <>
+//        {
+//
+//        };
+//        namespace internet_protocol
+        template <typename Application_layer, typename Transport_layer, typename Internet_layer, typename Link_layer>
+        struct internet_protocol
+        {
+            Application_layer application_layer;
+            Transport_layer transport_layer;
+            Internet_layer internet_layer;
+            Link_layer link_layer;
+        };
+        
+        template <typename Application_layer, typename Transport_layer, typename Internet_layer, typename Link_layer>
+        internet_protocol (Application_layer, Transport_layer, Internet_layer, Link_layer) ->  internet_protocol <Application_layer, Transport_layer, Internet_layer, Link_layer>;
+        
+        
+        
+        auto socket ()
+        {
+            return ::socket (AF_INET6, SOCK_STREAM, IPPROTO_TCP);
+        }
+        
+        
+//        template <template <typename...> typename Container = std::vector>
+        auto toIpAddresses (char const* str, auto& addresses)
+        {
+//            auto addresses = Container <char const*> {};
+            auto ipstr = (char*) std::malloc (INET6_ADDRSTRLEN * sizeof (char));
+            auto err = int {};
+
+            auto hints = addrinfo
+            {
+                .ai_family = AF_UNSPEC,
+                .ai_socktype = SOCK_STREAM
+            };
+            
+            addrinfo* res;
+            
+            if ((err = getaddrinfo (str, nullptr, &hints, &res)) != 0)
+            {
+                fprintf (stderr, "getaddrinfo: %s\n", gai_strerror(err));
+                exit (EXIT_FAILURE);
+            }
+            
+            for (auto* p = res; p != nullptr; p = p -> ai_next)
+            {
+                void* addr;
+                
+                switch (p -> ai_family)
+                {
+                    case AF_INET:
+                    {
+                        auto* ipv4 = (sockaddr_in*) p -> ai_addr;
+                        addr = &(ipv4 -> sin_addr);
+                        break;
+                    }
+                    case AF_INET6:
+                    {
+                        auto* ipv6 = (sockaddr_in6*) p -> ai_addr;
+                        addr = &(ipv6 -> sin6_addr);
+                        break;
+                    }
+                    default:
+                    {
+                        throw ("whaaat");
+                    }
+                }
+//                inet_ntop(<#int#>, <#const void *#>, <#char *#>, <#socklen_t#>)
+                inet_ntop (p -> ai_family, addr, ipstr, INET6_ADDRSTRLEN * sizeof (char));
+                addresses.push_back (ipstr);
+            }
+            
+            freeaddrinfo (res);
+            return addresses;
+        }
+    }
+    
+    
+    
+    
+    
+    namespace file
+    {
+        
+    }
+    
+    
+}
+
+
---
+++ tests/test_network.cpp
@@ -0,0 +1,284 @@
+#include <catch2/catch.hpp>
+#include <ph_network/network.hpp>
+using namespace std::literals::string_literals;
+
+template <typename T, auto x>
+std::ostream& operator<< (std::ostream& os, std::array<T, x> const& a)
+{
+    os << "{ ";
+    for (auto i : a)
+    {
+        os << i << " ";
+    }
+    os << "}";
+    
+    return os;
+}
+
+template <typename T>
+std::ostream& operator<< (std::ostream& os, std::vector<T> const& a)
+{
+    os << "{ ";
+    for (auto i : a)
+    {
+        os << i << " ";
+    }
+    os << "}";
+    
+    return os;
+}
+
+using namespace std;
+
+#define _PROCESS \
+    X(creation_unsuccessfull) \
+    X(child) \
+    X(anon_id)
+
+enum class PROCESS
+{
+#define X(p, ...) p,
+    _PROCESS
+#undef X
+};
+
+
+
+
+
+
+auto process (auto builder) -> PROCESS
+{
+//    auto p = fork ();
+    
+
+    
+}
+
+using std::cout;
+using std::endl;
+//using ph::operator==;
+
+auto operator"" _hej(char const*){
+    return 2;
+}
+
+TEST_CASE ("ph::keyboard")
+{
+    REQUIRE (ph::keyboard::backslash != 'k');
+    REQUIRE (ph::keyboard::backslash == '\\');
+    
+    REQUIRE ('k' != ph::keyboard::backslash);
+    REQUIRE ('\\' == ph::keyboard::backslash);
+}
+
+template <auto...>
+struct layer
+{
+    
+};
+
+consteval auto operator "" _transport_layer (char const* str, std::size_t n)
+{
+//    return 2;
+    int h = 0;
+    for(int i = 0; i < n; ++i)
+    {
+        ++h;
+    }
+    return h;
+}
+
+template <auto a>
+auto stri = std::string {a};
+
+
+template <typename T>
+concept Hash = std::is_same_v <std::hash, T> and requires (T t)
+{
+    true;
+};
+
+template <typename T>
+concept Nonce = requires (T, T) // nummer som bara används en gång
+{
+    requires not requires(T n1, T n2) {
+        n2 = n1;
+    };
+    true;
+};
+
+//template <typename >
+
+template <typename T>
+concept Node = requires ()
+{
+    true;
+};
+
+auto send (Node auto from, Node auto to)
+{
+    
+}
+
+auto contact (Node auto from, Node auto to)
+{
+    
+}
+
+
+auto challange (auto from, auto to) -> Nonce auto
+{
+    
+}
+
+auto respond (Hash auto, Node auto from, Node auto challanger)
+{
+    
+}
+
+auto hash_function (auto&& password, Nonce auto&&)
+{
+    return password;
+}
+
+
+TEST_CASE ("Server")
+{
+//    auto a = stri <"hej">;
+    
+//    auto a = "
+//    auto aa = "hej"_transport_layer;
+//    cout << aa << endl;
+//    auto aa = "hej"_hej;
+//    auto a = ph::network::internet_protocol {"hej", "hej", "hej", "hej"};
+//    auto a = ph::network::internet_protocol {.application_layer = "hej", .transport_layer = "hej", .internet_layer = "hej", .link_layer = "hej"};
+
+//    cout <<
+
+//    return;
+//    std::cout << "yoyoyo" << std::endl;
+//    auto addresses = std::vector <char const*> {};
+//    ph::network::toIpAddresses ("google.com", addresses);
+//    cout << addresses << endl;
+//    return;
+    
+    addrinfo* res;
+    
+    auto hints = addrinfo
+    {
+        .ai_family = AF_UNSPEC,
+        .ai_socktype = SOCK_STREAM,
+        .ai_flags = AI_PASSIVE
+    };
+    
+    if (auto err = getaddrinfo (nullptr, "80", &hints, &res); err != 0)
+    {
+        
+        fprintf (stderr, "getaddrinfo error: %s\n", gai_strerror (err));
+        exit (1);
+    }
+    
+    auto s = socket (res->ai_family, res->ai_socktype, res->ai_protocol);
+    
+    if (s == -1)
+    {
+        perror ("socket");
+        exit (1);
+    }
+    
+    fcntl (s, F_SETFL, O_NONBLOCK); // set to non-blocking
+    fcntl (s, F_SETFL, O_ASYNC); // set to asynchronous I/O
+    
+    auto yes = 1;
+    
+    if (auto err = setsockopt (s, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof (yes)); err == - 1)
+    {
+        perror ("sockopt");
+        exit (1);
+    }
+    
+//    char hostname[128];
+//    gethostname(hostname, sizeof hostname);
+//    printf("My hostname: %s\n", hostname);
+    
+    bind (s, res->ai_addr, res->ai_addrlen);
+    
+    if (listen (s, 10) == -1)
+    {
+        throw;
+    }
+    
+    
+    struct sockaddr_storage their_addr;
+    socklen_t addr_size {sizeof their_addr};
+    
+    auto their_socket = accept (s, (sockaddr*) &their_addr, &addr_size);
+    
+    if (their_socket < 0)
+    {
+        throw;
+    }
+    
+    std::byte buf [1024];
+    
+    
+    if (read (their_socket, buf, 1024) < 0)
+    {
+        throw;
+    }
+    
+    auto msg = "Hello from server.";
+    
+    write (their_socket, msg, strlen (msg));
+    
+    
+    
+    
+    
+//    auto addr_size = sizeof their_addr;
+    
+    freeaddrinfo (res);
+}
+
+TEST_CASE ("Client")
+{
+    return;
+    addrinfo* res;
+    
+    auto hints = addrinfo
+    {
+        .ai_family = AF_UNSPEC,
+        .ai_socktype = SOCK_STREAM,
+        .ai_flags = AI_PASSIVE
+    };
+    
+    if (auto err = getaddrinfo ("www.example.net", "8080", &hints, &res); err != 0)
+    {
+        
+        fprintf (stderr, "getaddrinfo error: %s\n", gai_strerror (err));
+        exit(1);
+    }
+    
+    auto s = socket (res->ai_family, res->ai_socktype, res->ai_protocol);
+    
+    if (auto err = connect (s, res->ai_addr, res->ai_addrlen); err == -1)
+    {
+        fprintf (stderr, "getaddrinfo error: %s\n", gai_strerror (err));
+        throw;
+        exit(1);
+    }
+    
+    
+    freeaddrinfo (res);
+}
+
+
+
+//
+//
+//
+TEST_CASE ("Constructs and assignments")
+{
+    std::cout << "hi" << std::endl;
+}
